0b4a4b8d5be68c7091d17a8c0ad87447228d1613
Weiqi Sun <weiqisun@gmail.com>
2020-01-12 22:19:56 -0500

    combine remote commands into one file

--- sender.py
+++ sender.py
@@ -2,15 +2,18 @@
 
 import sys
 import time
+import json
 import RPi.GPIO as GPIO
 
 def transmit(codes, pin):
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(pin, GPIO.OUT)
 
-    for s, t in codes:
-        GPIO.output(pin, s)
+    s = True
+    for t in codes:
+        GPIO.output(pin, int(s))
         time.sleep(t/1000000.)
+        s = not s
 
     GPIO.output(pin, 0)
     GPIO.cleanup()
@@ -21,7 +24,24 @@ def readSignalFile(filename):
     signal = [line.strip().split('\t') for line in lines]
     return [(int(s[0]), int(s[1])) for s in signal]
 
+def logConfig(config):
+    with open(config) as f:
+        remotes = json.load(f)
+    for remote in remotes.values():
+        for key in remote:
+            remote[key] = list(map(int, remote[key].split()))
+    return remotes
+
 if __name__ == "__main__":
-    signal_file = sys.argv[1]
+    remotes = logConfig(sys.argv[1])
     transmitter_pin = int(sys.argv[2])
-    transmit(readSignalFile(signal_file), transmitter_pin)
+    print("Load {} remotes: {}".format(len(remotes), ' '.join(remotes.keys())))
+    print("Transmitting on pin {}".format(transmitter_pin))
+
+    while True:
+        command = raw_input('Input remote and key (EXIT to stop) --> ')
+        if command == 'EXIT':
+            break
+        remote, key = command.split()
+        code = remotes[remote][key]
+        transmit(code, transmitter_pin)
